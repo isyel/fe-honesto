d4b8afe9eac6b1273b6c426c25bd205866380b66
isyel <isykaal4saviour@gmail.com>
2021-11-10 15:35:05 +0100

    update Button model, add disabled props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,20 +3,23 @@ import * as React from 'react'
 import classnames from 'classnames'
 
 type Props = {
-  onClick: (se: React.SyntheticEvent) => void
+  onClick: (se: React.SyntheticEvent, ...args: any) => void
   children: React.ReactNode
   secondary?: boolean
+  disabled?: boolean
 }
 
 const Button = (props: Props) => {
-  const { children, secondary, onClick } = props
+  const { children, secondary, disabled, onClick } = props
 
   return (
     <button
       className={classnames(styles.button, {
         [styles.secondaryButton]: secondary,
+        [styles.disabledButton]: disabled,
       })}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
