5aecdcdc6ddf20ff7f9899c359ccf0060f8f7bf8
isyel <isykaal4saviour@gmail.com>
2021-11-11 19:41:06 +0100

    🗳 add review component to handle reviews listing

---
+++ src/components/ReviewList/ReviewList.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react'
+import classNames from 'classnames'
+import styles from './scale.module.css'
+import FeedbackList from '../FeedbackList'
+import { ReviewItemT } from '../../context/ReviewerProvider'
+import { UserT } from '../../context/types'
+import User from '../User'
+import { FeedbackItemT } from '../../context/FeedbackProvider'
+
+type Props = {
+  userReviews: ReviewItemT[] | FeedbackItemT[] | any[]
+  selectedUser?: UserT | null
+  selectedFeedback?: ReviewItemT
+  handleSelectUser?: (user: UserT) => void
+}
+
+const ReviewList = (props: Props) => {
+  const { userReviews, selectedUser, selectedFeedback, handleSelectUser } =
+    props
+
+  return (
+    <>
+      <h1>Review Feedback Received</h1>
+      <div className={styles.feedbackContainer}>
+        <ul className={styles.users}>
+          <li>
+            <h3>Feedback Received</h3>
+          </li>
+          {userReviews.map((feedback, index) => (
+            <li
+              className={classNames(
+                styles.user,
+                selectedUser?.id ===
+                  (feedback.reviewer?.id || feedback.user?.id) &&
+                  styles.selected,
+              )}
+              key={`${Math.random}${index}`}
+            >
+              <User
+                id={feedback.reviewer?.id || feedback.user?.id}
+                name={feedback.reviewer?.name || feedback.user?.name}
+                avatarUrl={
+                  feedback.reviewer?.avatarUrl || feedback.user?.avatarUrl
+                }
+                handleOnClick={handleSelectUser}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <ul className={styles.feedback}>
+          <li>
+            <h2>{selectedUser?.name}'s Review</h2>
+          </li>
+          <FeedbackList feedbacks={selectedFeedback?.feedbacks} />
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default ReviewList
